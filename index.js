c52c84071fdf4ccb8709775944d8d925b5f05603
andrysiakigor2112-commits <andrysiakigor2112@gmail.com>
2026-08-23 17:22:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const http = require('http')
-
-const PORT = process.env.PORT
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Bot is running')
-})
-
-server.listen(PORT, () => {
-  console.log(`[Server] HTTP server started on port ${PORT}`)
-})
 "use strict";
 
 const { addLog, getLogs } = require("./logger");
